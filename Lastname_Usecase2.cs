0e09bdb0ed92811efd8c2f4295510984a5b530e5
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-04-06 17:24:19 +0530

    Validating Last Name Use case 2

---
+++ Lastname_Usecase2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_RegExp
+{
+    public class Lastname_Usecase2
+    {
+        public static string lastName = "^[A-Z]{1}[A-Za-z]{2}$";
+
+        public bool validatelastName(string lName)
+        {
+            return Regex.IsMatch(lName, lastName);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,21 @@
         {
             Console.WriteLine("Welcome to RegExp problems ");
             Console.WriteLine("******************************************************************");
-
+            //UseCase1
             Firstname_Usecase1 obj = new Firstname_Usecase1();
             Console.WriteLine("Write your FirstName ");
             string fName= Console.ReadLine();
             Console.WriteLine(obj.validatefirstName(fName));
+
+            Console.WriteLine("***********************************************************************");
+            //UseCase2
+            Lastname_Usecase2 obj2 = new Lastname_Usecase2();
+            Console.WriteLine("Write your LastName ");
+            string lName = Console.ReadLine();
+            Console.WriteLine(obj2.validatelastName(lName));
+
+            
+
         }
     }
 }
