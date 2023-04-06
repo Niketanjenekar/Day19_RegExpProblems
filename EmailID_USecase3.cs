f4e3b7e6c0605722ae9a01fb723827ae32a612f2
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-04-06 17:40:59 +0530

    Valdating Email ID Use case 3

---
+++ EmailID_USecase3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_RegExp
+{
+    public class EmailID_USecase3
+    {
+        public static string EmailId = "^[a-zA-Z0-9.!@#$%&*]+@[A-za-z0-9.]+[a-z]{3}$";
+
+        public bool validateEmailId(string emailId)
+        {
+            return Regex.IsMatch(emailId, EmailId);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,21 +5,27 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to RegExp problems ");
-            Console.WriteLine("******************************************************************");
-            //UseCase1
-            Firstname_Usecase1 obj = new Firstname_Usecase1();
-            Console.WriteLine("Write your FirstName ");
-            string fName= Console.ReadLine();
-            Console.WriteLine(obj.validatefirstName(fName));
+            //Console.WriteLine("******************************************************************");
+            ////UseCase1
+            //Firstname_Usecase1 obj = new Firstname_Usecase1();
+            //Console.WriteLine("Write your FirstName ");
+            //string fName= Console.ReadLine();
+            //Console.WriteLine(obj.validatefirstName(fName));
 
-            Console.WriteLine("***********************************************************************");
-            //UseCase2
-            Lastname_Usecase2 obj2 = new Lastname_Usecase2();
-            Console.WriteLine("Write your LastName ");
-            string lName = Console.ReadLine();
-            Console.WriteLine(obj2.validatelastName(lName));
+            //Console.WriteLine("***********************************************************************");
+            ////UseCase2
+            //Lastname_Usecase2 obj2 = new Lastname_Usecase2();
+            //Console.WriteLine("Write your LastName ");
+            //string lName = Console.ReadLine();
+            //Console.WriteLine(obj2.validatelastName(lName));
 
-            
+            Console.WriteLine("***************************************************************************");
+            //UseCase3
+            EmailID_USecase3 obj3 = new EmailID_USecase3();
+            Console.WriteLine("Please Enter your Email-Id");
+            string emaiID= Console.ReadLine();
+            Console.WriteLine(obj3.validateEmailId(emaiID));
+
 
         }
     }
